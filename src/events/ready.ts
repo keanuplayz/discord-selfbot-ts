3f63246566e068b6f32fa7fbf85207b6ec5462e4
Keanu <keanutimmermans8@gmail.com>
2020-10-23 09:50:28 +0000

    Still broken.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,14 +5,19 @@ import { Config } from '../core/structures';
 
 export default new Event<'ready'>({
   once() {
-    if (client.user) {
-      $.ready(
-        `Logged in as ${client.user.username}#${client.user.discriminator}.`,
-      );
-      client.user.setActivity({
-        type: 'LISTENING',
-        name: `${Config.prefix}help`,
-      });
-    }
+    // if (client.user) {
+    //   $.ready(
+    //     `Logged in as ${client.user.username}#${client.user.discriminator}.`,
+    //   );
+    //   client.user.setActivity({
+    //     type: 'LISTENING',
+    //     name: `${Config.prefix}help`,
+    //   });
+    // }
+        console.log(" ");
+        console.log("*---------------------*");
+        console.log("Started AutoReply for " + client.user?.tag);
+        console.log(`Prefix is: ${Config.prefix}`);
+        client.user?.setStatus("online");
   },
 });
